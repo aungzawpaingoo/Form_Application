9c6d2eca472f67549acb03a8bcd9f38c0341a7e6
Aung Zaw Paing Oo <aungzawpaingoonetwork@gmail.com>
2024-03-23 23:47:24 +0630

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useForm} from 'react-hook-form'
+
 
 function App() {
+
+  const form = useForm();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+    <h1>Form Application</h1>
+
+    <label htmlFor='username'>Username</label>
+    <input type='text' id='username' name='username' />
+
+    <label htmlFor='email'>Email</label>
+    <input type='email' id='email' name='email' />
+
+    <label htmlFor='channel'>Channel Name</label>
+    <input type='text' id='channel' name='channel' />
+
+    <button>Submit</button>
+
+
     </div>
-  );
+
+
+  )
 }
 
-export default App;
+export default App
